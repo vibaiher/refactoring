dae4ea94a89a72e5f3d440dc706fc365894ae51a
Vicente Baixauli <vibaiher@gmail.com>
2015-02-25 11:43:57 +0100

    CH1. EX1. Apply Move Method

--- chapter-1/example-1/Rental.java
+++ chapter-1/example-1/Rental.java
@@ -20,9 +20,6 @@ class Rental {
   }
 
   int getFrequentRenterPoints() {
-    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-      return 2;
-    else
-      return 1;
+    return _movie.getFrequentRenterPoints(_daysRented);
   }
 }
