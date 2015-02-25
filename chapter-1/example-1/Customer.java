4c050a3a95b46b1da4129673346e28ad0e020841
Vicente Baixauli <vibaiher@gmail.com>
2015-02-25 10:38:13 +0100

    CH1. EX1. Apply Replace Temp with Query

--- chapter-1/example-1/Customer.java
+++ chapter-1/example-1/Customer.java
@@ -20,10 +20,7 @@ class Customer {
     Enumeration rentals = _rentals.elements();
     String result = "Rental Record for " + getName() + "\n";
     while (rentals.hasMoreElements()) {
-      double thisAmount = 0;
       Rental each = (Rental) rentals.nextElement();
-
-      thisAmount = each.getCharge();
 
       // add frequent renter points
       frequentRenterPoints ++;
@@ -32,8 +29,8 @@ class Customer {
         frequentRenterPoints ++;
 
       //show figures for this rental
-      result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+      result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
+      totalAmount += each.getCharge();
 
     }
     //add footer lines
