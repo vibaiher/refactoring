910954c188c7aecefdf8ff221d479f346019c1c7
Vicente Baixauli <vibaiher@gmail.com>
2015-02-25 10:44:10 +0100

    CH1. EX1. Apply Extract Method

--- chapter-1/example-1/Rental.java
+++ chapter-1/example-1/Rental.java
@@ -34,4 +34,11 @@ class Rental {
     }
     return result;
   }
+
+  int getFrequentRenterPoints() {
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+      return 2;
+    else
+      return 1;
+  }
 }
